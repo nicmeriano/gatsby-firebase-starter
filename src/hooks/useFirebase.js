1d15a17e89578173e58b916bac1b7095c6ba56d6
nicmeriano <nicmeriano@gmail.com>
2019-11-21 16:54:55 -0800

    prettier config

--- src/hooks/useFirebase.js
+++ src/hooks/useFirebase.js
@@ -1,9 +1,9 @@
-import React, { useState, useEffect, useContext, createContext } from "react"
-import * as firebase from "firebase/app"
-import "firebase/auth"
-import "firebase/firestore"
+import React, { useState, useEffect, useContext, createContext } from "react";
+import * as firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
 
-let app
+let app;
 
 // don't initialize untill you're in the browser - required to work with Gatsby
 if (typeof window !== "undefined") {
@@ -15,39 +15,39 @@ if (typeof window !== "undefined") {
     storageBucket: process.env.GATSBY_FIREBASE_STORAGE_BUCKET,
     messagingSenderId: process.env.GATSBY_FIREBASE_MESSAGING_ID,
     appId: process.env.GATSBY_FIREBASE_APP_ID,
-  })
+  });
 }
 
-const FirebaseContext = createContext({ auth: {}, firebase: {} })
+const FirebaseContext = createContext({ auth: {}, firebase: {} });
 
 // Provider component that wraps your app and makes auth object ...
 // ... available to any child component that calls useAuth().
 export function ProvideFirebase({ children }) {
-  const firebase = useProvideFirebase()
+  const firebase = useProvideFirebase();
   return (
     <FirebaseContext.Provider value={firebase}>
       {children}
     </FirebaseContext.Provider>
-  )
+  );
 }
 
 // Hook for child components to get the auth/firestore object ...
 // ... and re-render when it changes.
 export const useFirebase = () => {
-  return useContext(FirebaseContext)
-}
+  return useContext(FirebaseContext);
+};
 
 // Provider hook that creates auth object and handles state
 function useProvideFirebase() {
-  const [user, setUser] = useState(undefined) // set as undefined to let components know whether it has been initialized (to prevent flashing page redirects)
+  const [user, setUser] = useState(undefined); // set as undefined to let components know whether it has been initialized (to prevent flashing page redirects)
 
   const sendEmailVerification = user => {
     if (user) {
       user.sendEmailVerification().catch(e => {
-        throw new Error(e.message)
-      })
+        throw new Error(e.message);
+      });
     }
-  }
+  };
 
   // Wrap any Firebase methods we want to use making sure ...
   // ... to save the user to state.
@@ -56,73 +56,73 @@ function useProvideFirebase() {
       .auth()
       .signInWithEmailAndPassword(email, password)
       .then(response => {
-        setUser(response.user)
-        return response.user
-      })
-  }
+        setUser(response.user);
+        return response.user;
+      });
+  };
 
   const signInWithGoogle = () => {
-    return app.auth().signInWithPopup(new firebase.auth.GoogleAuthProvider())
+    return app.auth().signInWithPopup(new firebase.auth.GoogleAuthProvider());
     // TODO: Add error handling as described in docs https://firebase.google.com/docs/auth/web/google-signin
-  }
+  };
   const signInWithFacebook = () => {
-    return app.auth().signInWithPopup(new firebase.auth.FacebookAuthProvider())
+    return app.auth().signInWithPopup(new firebase.auth.FacebookAuthProvider());
     // TODO: Add error handling as described in docs https://firebase.google.com/docs/auth/web/google-signin
-  }
+  };
 
   const signup = (email, password) => {
     return app
       .auth()
       .createUserWithEmailAndPassword(email, password)
       .then(({ user }) => {
-        setUser(user)
-        sendEmailVerification(user)
-        return user
-      })
-  }
+        setUser(user);
+        sendEmailVerification(user);
+        return user;
+      });
+  };
 
   const signout = () => {
     return app
       .auth()
       .signOut()
       .then(() => {
-        setUser(null)
-      })
-  }
+        setUser(null);
+      });
+  };
 
   const sendPasswordResetEmail = email => {
     return app
       .auth()
       .sendPasswordResetEmail(email)
       .then(() => {
-        return true
-      })
-  }
+        return true;
+      });
+  };
 
   const confirmPasswordReset = (code, password) => {
     return app
       .auth()
       .confirmPasswordReset(code, password)
       .then(() => {
-        return true
-      })
-  }
+        return true;
+      });
+  };
 
   // Subscribe to user on mount
   // Because this sets state in the callback it will cause any ...
   // ... component that utilizes this hook to re-render with the ...
   // ... latest auth object.
   useEffect(() => {
-    console.log("use effect useFirebase start")
+    console.log("use effect useFirebase start");
     const unsubscribe = app.auth().onAuthStateChanged(user => {
       if (user) {
-        setUser(user)
+        setUser(user);
       } else {
-        setUser(null)
+        setUser(null);
       }
-    })
-    return () => unsubscribe()
-  }, [])
+    });
+    return () => unsubscribe();
+  }, []);
 
   return {
     auth: {
@@ -136,5 +136,5 @@ function useProvideFirebase() {
       signInWithFacebook,
     },
     firestore: firebase.firestore(),
-  }
+  };
 }
